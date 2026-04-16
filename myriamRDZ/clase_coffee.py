97d665200191ae0ede0cd67b12fbd72689bc7fd9
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-04-16 01:21:35 -0600

    Añadido atributos de clase

--- myriamRDZ/clase_coffee.py
+++ myriamRDZ/clase_coffee.py
@@ -1,3 +1,7 @@
 class Coffee:
-    pass
-    
+    def __init__(self, nombre, tipo_grano, ID, tipo_temperatura, precio):
+        self.nombre = nombre
+        self.tipo_grano = tipo_grano
+        self.ID = ID
+        self.tipo_temperatura = tipo_temperatura
+        self.precio = precio
