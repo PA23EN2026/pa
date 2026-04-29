a40559fb74383824a8e009ccb5c1a224fee96821
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-29 17:04:09 -0600

    Se creoel metodo sumar y con una prueba de que si funciona

--- JeanGarza/EV3.py
+++ JeanGarza/EV3.py
@@ -1,6 +1,12 @@
 class Mi_Clase:
-    def __init__(num1,num2,num3):
-        num1 = 1
-        num2 = 2
-        num3 = 3
-pass
+    def __init__(self,num1,num2,num3):
+        self.__num1 = num1
+        self.__num2 = num2
+        self.__num3 = num3
+    
+    def sumar(self):
+        resultado = self.__num1 + self.__num2 + self.__num3
+        print(f"El resultado de la suma de esos tres numero es: {resultado}")
+
+Objetosuma = Mi_Clase(2,5,10)
+Objetosuma.sumar()
