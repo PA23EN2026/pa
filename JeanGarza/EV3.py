631f5ce883de75c54c295dd2d7428ea09e2d7444
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-29 16:37:52 -0600

    Se creo la clase con innit

--- JeanGarza/EV3.py
+++ JeanGarza/EV3.py
@@ -1,5 +1,6 @@
-print("SUMA")
-numero1 = int(input("Dime el primer numero a sumar: "))
-numero2= int(input("Dime el segundo numero a sumar: "))
-
-print(f"La suma es: {numero1 + numero2}") 
+class Mi_Clase:
+    def __init__(num1,num2,num3):
+        num1 = 1
+        num2 = 2
+        num3 = 3
+pass
