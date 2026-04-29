823dd926071d0f44d528a5fc5756a26f08f49abc
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-29 17:11:58 -0600

    Se creoel getter (property) para el numero 1

--- JeanGarza/EV3.py
+++ JeanGarza/EV3.py
@@ -7,6 +7,13 @@ class Mi_Clase:
     def sumar(self):
         resultado = self.__num1 + self.__num2 + self.__num3
         print(f"El resultado de la suma de esos tres numero es: {resultado}")
+     
+    @property
+    def num1 (self):
+        return self.__num1
+    
+
 
 Objetosuma = Mi_Clase(2,5,10)
 Objetosuma.sumar()
+print(Objetosuma.num1)
