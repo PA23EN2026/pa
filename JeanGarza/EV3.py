d6f0eedeecebbb757b17d57ae8973aa113daaec8
Jean Carlos GR <jeangarza12@gmail.com>
2026-04-29 17:24:49 -0600

    Se creo la funcion mayor y un nuevo objeto para probar la funcion

--- JeanGarza/EV3.py
+++ JeanGarza/EV3.py
@@ -8,12 +8,26 @@ class Mi_Clase:
         resultado = self.__num1 + self.__num2 + self.__num3
         print(f"El resultado de la suma de esos tres numero es: {resultado}")
      
+    def mayor(self):
+        if self.__num1 > self.__num2 and self.__num1 > self.__num3:
+            print(f"El numero mayor es: {self.__num1}")
+        
+        elif self.__num2 > self.__num3:
+            print(f"El número mayor es: {self.__num2}")
+        
+        else:
+            print(f"El número mayor es: {self.__num3}")
+
+
     @property
     def num1 (self):
         return self.__num1
     
 
 
-Objetosuma = Mi_Clase(2,5,10)
-Objetosuma.sumar()
-print(Objetosuma.num1)
+objetosuma = Mi_Clase(2,5,10)
+objetosuma.sumar()
+print(objetosuma.num1)
+
+objetomayor = Mi_Clase(1,10,100)
+objetomayor.mayor()
