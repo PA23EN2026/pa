f972d904ba8563b3b541ce25e84ff725b5fb6313
EvelynHP <evelyn.hernandezpr@uanl.edu.mx>
2026-04-29 18:10:00 -0600

    Se agrego el metodo str

--- evelynhp/practica2.py
+++ evelynhp/practica2.py
@@ -28,6 +28,11 @@ class MI_clase:
             return True
         else:
             return False 
+    
+    def concatenar(self):
+        return str(self.__num1) + str(self.__num2) + str(self.__num3)
+    
+    
         
         
         
