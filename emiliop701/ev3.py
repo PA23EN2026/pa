a93c01a967fc8b45725d8924e964e8ac65ed4261
Emilio Puente <puenteemilio48@gmail.com>
2026-05-01 15:06:20 -0600

    se crea el metodo de suma

--- emiliop701/ev3.py
+++ emiliop701/ev3.py
@@ -3,3 +3,8 @@ class Mi_clase:
         self.__num1=num1
         self.__num2=num2
         self.__num3=num3
+
+def sumar(self):
+    result= self.__num1 + self.__num2 + self.__num3 
+    return result
+
