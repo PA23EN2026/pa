3e9fb8e6ecbeb599c0974cb73b8e346a00831863
osmarrdaniel <osmar.avalost@uanl.edu.mx>
2026-05-01 14:07:56 -0600

    Se creo metodo sumar

--- osmarrdaniel/practica2.py
+++ osmarrdaniel/practica2.py
@@ -3,4 +3,7 @@ class Mi_Clase:
         self.__num1 = num1
         self.__num2 = num2
         self.__num3 = num3
+    
+    def sumar(self):
+        return self.__num1 + self.__num2 + self.__num3
         
