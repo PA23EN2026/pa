7caa2ebf9bd2551071a00f1ac2856f786cb11797
Emilio Puente <puenteemilio48@gmail.com>
2026-05-01 15:59:39 -0600

    se piden los datos

--- emiliop701/ev3.py
+++ emiliop701/ev3.py
@@ -4,30 +4,35 @@ class Mi_clase:
         self.__num2=num2
         self.__num3=num3
 
-def sumar(self):
-    result= self.__num1 + self.__num2 + self.__num3 
-    return result
+    def sumar(self):
+        result= self.__num1 + self.__num2 + self.__num3 
+        return result
 
-def mayor(self):
-    if (self.__num1 > self._num2) and (self.__num1 > self.__num3):
-        print(f"El numero mayor es {self.__num1}")
-    elif (self.__num2 > self.__num1) and (self.__num2 > self.__num3):
-        print(f"El numero mayor es {self.__num2}")
-    else:
-        print(f"El numero mayor es {self.__num3}")
+    def mayor(self):
+        if (self.__num1 > self._num2) and (self.__num1 > self.__num3):
+            print(f"El numero mayor es {self.__num1}")
+        elif (self.__num2 > self.__num1) and (self.__num2 > self.__num3):
+            print(f"El numero mayor es {self.__num2}")
+        else:
+            print(f"El numero mayor es {self.__num3}")
 
-def menor(self):
-    if (self.__num1 < self._num2) and (self.__num1 < self.__num3):
-        print(f"El numero menor es {self.__num1}")
-    elif (self.__num2 < self.__num1) and (self.__num2 < self.__num3):
-        print(f"El numero menor es {self.__num2}")
-    else:
-        print(f"El numero menor es {self.__num3}")
+    def menor(self):
+        if (self.__num1 < self._num2) and (self.__num1 < self.__num3):
+            print(f"El numero menor es {self.__num1}")
+        elif (self.__num2 < self.__num1) and (self.__num2 < self.__num3):
+            print(f"El numero menor es {self.__num2}")
+        else:
+            print(f"El numero menor es {self.__num3}")
 
-def iguales(self):
-    result=(self.__num1==self.__num2) and (self.__num2==self.__num3)
-    return result 
+    def iguales(self):
+        result=(self.__num1==self.__num2) and (self.__num2==self.__num3)
+        return result 
 
-def concatenar(self):
-    texto=str(self.__num1)+str(self.__num2)+str(self.__num3)
-    return texto 
+    def concatenar(self):
+        texto=str(self.__num1)+str(self.__num2)+str(self.__num3)
+        return texto 
+
+
+num1=int(input("Ingresa un numero"))
+num2=int(input("Ingresa un numero"))
+num3=int(input("Ingresa un numero"))
