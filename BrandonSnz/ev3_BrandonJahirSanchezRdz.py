6ffd9e52dd53e12138b8fdebcc11f09883877a57
Brandon Jahir Sanchez Rodriguez <youngflow299@gmail.com>
2026-05-01 17:13:24 -0600

    Creacion de metodos no terminados

--- BrandonSnz/ev3_BrandonJahirSanchezRdz.py
+++ BrandonSnz/ev3_BrandonJahirSanchezRdz.py
@@ -23,3 +23,32 @@ class Mi_Clase:
     def set_num3(self, valor):
         self.num3 = valor
 
+    def suma(self):
+        return self.num1 + self.num2 + self.num3
+
+
+    def nummayor(self):
+        if self.num1 >= self.num2 and self.num1 >= self.num3:
+            return self.num1
+        elif self.num2 >= self.num1 and self.num2 >= self.num3:
+            return self.num2
+        else:
+            return self.num3
+
+
+
+    def  num_menor(self):
+        if self.num1 <= self.num2 and self.num1 <= self.num3:
+            return self.num1
+        elif self.num2 <= self.num1 and self.num2 <= self.num3:
+            return self.num2
+        else:
+            return self.__num3
+
+
+    def num_iguales(self):
+        pass
+
+
+    def concatenar(self):
+        pass
