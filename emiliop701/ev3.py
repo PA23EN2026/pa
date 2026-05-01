c351288d84ddc74cdb220e720d598ce524eddf0d
Emilio Puente <puenteemilio48@gmail.com>
2026-05-01 15:33:12 -0600

    se crea el metodo de menor

--- emiliop701/ev3.py
+++ emiliop701/ev3.py
@@ -16,3 +16,10 @@ def mayor(self):
     else:
         print(f"El numero mayor es {self.__num3}")
 
+def menor(self):
+    if (self.__num1 < self._num2) and (self.__num1 < self.__num3):
+        print(f"El numero menor es {self.__num1}")
+    elif (self.__num2 < self.__num1) and (self.__num2 < self.__num3):
+        print(f"El numero menor es {self.__num2}")
+    else:
+        print(f"El numero menor es {self.__num3}")
