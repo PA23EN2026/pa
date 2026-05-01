081d6f1f7021c6bdb5bfadc729371e4d7ad9cd3e
osmarrdaniel <osmar.avalost@uanl.edu.mx>
2026-05-01 14:28:21 -0600

    Se creo metodo menor

--- osmarrdaniel/practica2.py
+++ osmarrdaniel/practica2.py
@@ -14,4 +14,11 @@ class Mi_Clase:
             return self.__num2
         else:
             return self.__num3
-            
+    
+    def menor(self):
+        if self.__num1 <= self.__num2 and self.__num1 <= self.__num3:
+            return self.__num1
+        elif self.__num2 <= self.__num1 and self.__num2 <= self.__num3:
+            return self.__num2
+        else:
+            return self.__num3        
