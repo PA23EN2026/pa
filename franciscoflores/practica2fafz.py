12637737d5a8a09d5b1a6b3194350359a79db0aa
Francisco Flores <notdemononebv@gmail.com>
2026-05-02 16:33:44 -0600

    Se agrego método mayor

--- franciscoflores/practica2fafz.py
+++ franciscoflores/practica2fafz.py
@@ -3,6 +3,14 @@ class Mi_Clase:
         self.__num1 = num1
         self.__num2 = num2
         self.__num3 = num3
-        
+
      def sumar(self):
          return self.__num1 + self.__num2 + self.__num3
+
+     def mayor(self):
+         if self.__num1 >= self.__num2 and self.__num1 >= self.__num3:
+             return self.__num1
+         elif self.__num2 >= self.__num3:
+             return self.__num2
+         else:
+             return self.__num3
