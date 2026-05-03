b032a25aa36927f13c49bc3e9fdf04ded8865d61
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-05-03 11:00:20 -0600

    Prueba de clase

--- myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
+++ myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
@@ -9,33 +9,43 @@ class Numeros:
         return f"suma: {suma}"
     
     def mayor(self):
-        if self.numero1 > self.numero2 and self.numero1 > self.numero3:
-            print(f"El numero {self.numero1} es mayor")
-        elif self.numero2 > self.numero1 and self.numero2 > self.numero3:
-            print(f"El numero {self.numero2} es mayor")
-        elif self.numero3 > self.numero1 and self.numero3 > self.numero2:
-            print(f"El numero {self.numero3} es mayor")
+        if self.__numero1 > self.__numero2 and self.__numero1 > self.__numero3:
+            return f"El numero {self.__numero1} es mayor"
+        elif self.__numero2 > self.__numero1 and self.__numero2 > self.__numero3:
+            return f"El numero {self.__numero2} es mayor"
+        elif self.__numero3 > self.__numero1 and self.__numero3 > self.__numero2:
+            return f"El numero {self.__numero3} es mayor"
             
     def menor(self):
-        if self.numero1 < self.numero2 and self.numero1 < self.numero3:
-            print(f"El numero {self.numero1} es menor")
-        elif self.numero2 < self.numero1 and self.numero2 < self.numero3:
-            print(f"El numero {self.numero2} es menor")
-        elif self.numero3 < self.numero1 and self.numero3 < self.numero2:
-            print(f"El numero {self.numero3} es menor")
+        if self.__numero1 < self.__numero2 and self.__numero1 < self.__numero3:
+            return f"El numero {self.__numero1} es menor" 
+        elif self.__numero2 < self.__numero1 and self.__numero2 < self.__numero3:
+            return f"El numero {self.__numero2} es menor"
+        elif self.__numero3 < self.__numero1 and self.__numero3 < self.__numero2:
+            return f"El numero {self.__numero3} es menor"
             
     def iguales(self):
-        igual1y2 = self.numero1 == self.numero2
-        igual1y3 = self.numero1 == self.numero3
-        igual2y3 = self.numero2 == self.numero3
-        todoigual = self.numero1 == self.numero2 = self.numero3
+        igual1y2 = self.__numero1 == self.__numero2
+        igual1y3 = self.__numero1 == self.__numero3
+        igual2y3 = self.__numero2 == self.__numero3
+        todoigual = self.__numero1 == self.__numero2 == self.__numero3
         
         return f"Numero 1 y 2 son iguales: {igual1y2}, Numero 1 y 3 son iguales: {igual1y3}, Numero 2 y 3 son iguales: {igual2y3}, Todos son iguales: {todoigual}"
     
     def concatenar(self):
-        resultado = str(self.numero1) + str(self.numero2) + str(self.numero3)
+        resultado = str(self.__numero1) + str(self.__numero2) + str(self.__numero3)
         return resultado
-      
     
+# Prueba de clase
+    
+numeros1 = Numeros(33, 50, 1)
+print("numeros: 33, 50, 1")
+print(numeros1.sumar())
+print(numeros1.mayor())
+print(numeros1.menor())
+print(numeros1.concatenar())
 
-        
+print("---------------------------")
+print("numeros: 11, 11, 10")
+numeros2 = Numeros(11, 11, 10)
+print(numeros2.iguales())
