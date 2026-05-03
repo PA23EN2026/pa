7c7ec1b2a9fad1266fc70e530b250723d4e0e451
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-05-02 22:23:47 -0600

    Metodo mayor añadido

--- myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
+++ myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
@@ -1,12 +1,20 @@
 class Numeros:
     def __init__(self, numero1, numero2, numero3):
-        self.numero1 = numero1
-        self.numero2 = numero2
-        self.numero3 = numero3
+        self.__numero1 = numero1
+        self.__numero2 = numero2
+        self.__numero3 = numero3
     
     def sumar(self):
-        suma = self.numero1 + self.numero2 + self.numero3
+        suma = self.__numero1 + self.__numero2 + self.__numero3
         return f"suma: {suma}"
+    
+    def mayor(self):
+        if self.numero1 > self.numero2 and self.numero1 > self.numero3:
+            print(f"El numero {self.numero1} es mayor")
+        elif self.numero2 > self.numero1 and self.numero2 > self.numero3:
+            print(f"El numero {self.numero2} es mayor")
+        elif self.numero3 > self.numero1 and self.numero3 > self.numero2:
+            print(f"El numero {self.numero3} es mayor")
     
 
         
