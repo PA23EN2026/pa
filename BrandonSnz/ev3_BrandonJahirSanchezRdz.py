a0e1baed84b6bf6a70198923fc2f4467b4f5dba8
Brandon Jahir Sanchez Rodriguez <youngflow299@gmail.com>
2026-05-02 18:32:10 -0600

    Prueba de clase y metodos

--- BrandonSnz/ev3_BrandonJahirSanchezRdz.py
+++ BrandonSnz/ev3_BrandonJahirSanchezRdz.py
@@ -56,3 +56,18 @@ class Mi_Clase:
 
     def concatenar(self):
         return (f"{self.__num1}{self.__num2}{self.__num3}")
+    
+
+
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el primer numero: "))
+num3 = int(input("Ingresa el primer numero: "))
+
+objeto_num = Mi_Clase(num1, num2, num3)
+
+
+print(f"La suma es: {objeto_num.suma()}")
+print(f"Numero mayor: {objeto_num.nummayor()}")
+print(f"Numero menor: {objeto_num.num_menor()}")
+print(f"Numeros iguales: {objeto_num.num_iguales()}")
+print(f"Concatenar: {objeto_num.concatenar()}")
