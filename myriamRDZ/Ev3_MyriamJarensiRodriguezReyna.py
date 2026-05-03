230f1cfa2e2c61735b4be0ba73262db28f8bc7ab
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-05-02 22:12:34 -0600

    Añadido tributos

---
+++ myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
@@ -0,0 +1,7 @@
+class Funcion_Aritmetica:
+    def __init__(self, numero1, numero2, numero3):
+        self.numero1 = numero1
+        self.numero2 = numero2
+        self.numero3 = numero3
+    
+    
