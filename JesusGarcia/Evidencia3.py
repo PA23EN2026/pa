3f2c00e162bbb0d9ec5db9fc7330731357bcdc3a
Jesus Garcia <superfrenos@MacBook-Pro-de-Jesus.local>
2026-05-03 14:13:54 -0600

    usando el metodo de concatenar le agregamos el string

--- JesusGarcia/Evidencia3.py
+++ JesusGarcia/Evidencia3.py
@@ -28,3 +28,6 @@ class Mi_Clase:
             return True
         else:
             return False
+        
+    def concatenar(self):
+        return str(self.__num1) + str(self.__num2) + str(self.__num3)
