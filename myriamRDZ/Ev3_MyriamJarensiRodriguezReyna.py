605daddfa2c34dd8f158891369335a7f0de9c67f
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-05-03 10:32:04 -0600

    Metodo igual añadido

--- myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
+++ myriamRDZ/Ev3_MyriamJarensiRodriguezReyna.py
@@ -23,6 +23,16 @@ class Numeros:
             print(f"El numero {self.numero2} es menor")
         elif self.numero3 < self.numero1 and self.numero3 < self.numero2:
             print(f"El numero {self.numero3} es menor")
+            
+    def iguales(self):
+        igual1y2 = self.numero1 == self.numero2
+        igual1y3 = self.numero1 == self.numero3
+        igual2y3 = self.numero2 == self.numero3
+        todoigual = self.numero1 == self.numero2 = self.numero3
+        
+        return f"Numero 1 y 2 son iguales: {igual1y2}, Numero 1 y 3 son iguales: {igual1y3}, Numero 2 y 3 son iguales: {igual2y3}, Todos son iguales: {todoigual}"
+    
+      
     
 
         
